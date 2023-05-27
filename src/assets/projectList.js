3e10e1b33137c7b7adf3b3cac813a81770c85f40
Vio Szabo <vio@vioszabo.se>
2023-05-27 15:24:19 +0200

    add projects

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -1,114 +1,133 @@
-
 const projectList = [
-    {
-        name: 'Labyrinth game',
-        img: '../img/captainsadventure.png',
-        alt: 'screenshot of the site',
-        description: 'A pair project, working on APIs with Redux for an exploration game.',
-        tags: ['Redux', 'React', 'API', 'CSS', 'pair-programming'],
-        demo: 'https://captains-adventure.netlify.app/',
-        code: 'https://github.com/code-and-cats/project-labyrinth',
-    },
-    {
-        name: 'Todo-list in React',
-        img: '../img/checkit.png',
-        alt: 'screenshot of the app',
-        description: 'To do-list made with React and Redux, working with reducers and actions.',
-        tags: ['Redux', 'React', 'CSS'],
-        demo: 'https://vios-todo.netlify.app/',
-        code: 'https://github.com/code-and-cats/project-todos',
-    },
-    {
-        name: 'Quiz',
-        img: '../img/quiz.png',
-        alt: 'the quiz shown on mobile',
-        description: 'A 100% serious quiz about the front side of Sweden (Gothenburg) made in React.',
-        tags: ['Redux', 'CSS', 'mob-programming'],
-        demo: 'https://deluxe-gelato-680ed8.netlify.app/',
-        code: 'https://github.com/Technigo/project-redux-quiz/pull/107',
-    },
-    {
-        name: 'Design hand off',
-        img: '../img/radiantroots.png',
-        alt: 'the site shown on mobile, tablet and desktop',
-        description: 'Implemented a design made by a UX student for a ficitional yoga studio. A deep dive into styled components.',
-        tags: ['React', 'CSS'],
-        demo: 'https://spectacular-pudding-f8c7fa.netlify.app/',
-        code: 'https://github.com/code-and-cats/project-design-handoff',
-    },
-    {
-        name: 'Happy thoughts',
-        img: '../img/happythoughts.png',
-        alt: 'the site shown on mobile',
-        description: 'A twitter-like site, but with happy thoughts only. Practicing API fetches and following a design.',
-        tags: ['React', 'CSS'],
-        demo: 'https://enchanting-halva-88064a.netlify.app/',
-        code: 'https://github.com/code-and-cats/project-happy-thoughts',
-    },
-    {
-        name: 'Pet shrimp order form',
-        img: '../img/shrimporder.png',
-        alt: 'the site shown on mobile',
-        description: 'A small order form to practice React states. Order your shrimp and get a confirmation!',
-        tags: ['React', 'CSS'],
-        demo: 'https://astounding-starship-c864d5.netlify.app/',
-        code: 'https://github.com/code-and-cats/project-survey',
-    },
-    {
-        name: 'Music releases',
-        img: '../img/musicreleases.png',
-        alt: 'the site shown on tablet',
-        description: 'Spotify clone fetching music releases from an open API. The 50 latest music releases... from then.',
-        tags: ['React', 'CSS', 'Pair-programming'],
-        demo: 'https://vio-and-fridas-music-release-project.netlify.app/',
-        code: 'https://github.com/Technigo/project-music-releases/pull/251',
-    },
-    {
-        name: 'Business site - Dynamic Duo',
-        img: '../img/dd.png',
-        alt: 'the site shown on desktop and mobile',
-        description: 'Content consulting and styling of a Wordpress site for an experienced event company in Sweden.',
-        tags: ['Wordpress'],
-        demo: 'http://dynamicduo.se/',
-        code: 'http://dynamicduo.se/',
-    },
-    {
-        name: 'Weather app',
-        img: '../img/weatherapp.png',
-        alt: 'hand holding a mobile phone with the app on it',
-        description: 'The weather app fetches weather data from whichever city the user chooses (but will always start in Reykjavik).',
-        tags: ['HTML', 'JavaScript', 'CSS', 'Pair-programming'],
-        demo: 'https://comfy-weatherapp-ea57d9.netlify.app/',
-        code: 'https://github.com/code-and-cats/project-weather-app',
-    },
-    {
-        name: 'Guess who - the game',
-        img: '../img/guesswoof.png',
-        alt: 'plant bot on a laptop with a leaf of a plant in a glass of water next to it',
-        description: 'A take on the board game "guess who" - but with dogs. Wearing hats.',
-        tags: ['HTML', 'JavaScript', 'CSS'],
-        demo: 'https://chipper-nasturtium-c0cb39.netlify.app/',
-        code: 'https://github.com/code-and-cats/project-guess-who',
-    },
-    {
-        name: 'Chatbot built in JavaScript',
-        img: '../img/plantbot.png',
-        alt: 'the game shown on desktop and mobile',
-        description: 'A conversational tool to help the user choose the perfect plant.',
-        tags: ['HTML', 'JavaScript', 'CSS'],
-        demo: 'https://vermillion-kashata-dd952d.netlify.app/',
-        code: 'https://github.com/code-and-cats/project-chatbot',
-    },
-    {
-        name: 'News site',
-        img: '../img/dailymews.png',
-        alt: 'the site shown on desktop, mobile and tablet',
-        description: 'My first project working with responsive cards, flexbox and grid.',
-        tags: ['HTML', 'CSS'],
-        demo: 'https://earnest-sorbet-0d6a88.netlify.app/',
-        code: 'https://github.com/code-and-cats/project-news-site',
-    }
-   
+  {
+    name: 'Randomizer',
+    img: '../img/randomizer.png',
+    alt: 'screenshot of the site',
+    description: 'A small personal project for practicing React.',
+    tags: ['React', 'styled-components', 'CSS'],
+    demo: 'https://random-randomizer.netlify.app/',
+    code: 'https://github.com/code-and-cats/random-randomizer'
+  },
+  {
+    name: 'Labyrinth game',
+    img: '../img/captainsadventure.png',
+    alt: 'screenshot of the site',
+    description:
+      'A pair project, working on APIs with Redux for an exploration game.',
+    tags: ['Redux', 'React', 'API', 'CSS', 'pair-programming'],
+    demo: 'https://captains-adventure.netlify.app/',
+    code: 'https://github.com/code-and-cats/project-labyrinth'
+  },
+  {
+    name: 'Todo-list in React',
+    img: '../img/checkit.png',
+    alt: 'screenshot of the app',
+    description:
+      'To do-list made with React and Redux, working with reducers and actions.',
+    tags: ['Redux', 'React', 'CSS'],
+    demo: 'https://vios-todo.netlify.app/',
+    code: 'https://github.com/code-and-cats/project-todos'
+  },
+  {
+    name: 'Quiz',
+    img: '../img/quiz.png',
+    alt: 'the quiz shown on mobile',
+    description:
+      'A 100% serious quiz about the front side of Sweden (Gothenburg) made in React.',
+    tags: ['Redux', 'CSS', 'mob-programming'],
+    demo: 'https://deluxe-gelato-680ed8.netlify.app/',
+    code: 'https://github.com/Technigo/project-redux-quiz/pull/107'
+  },
+  {
+    name: 'Design hand off',
+    img: '../img/radiantroots.png',
+    alt: 'the site shown on mobile, tablet and desktop',
+    description:
+      'Implemented a design made by a UX student for a ficitional yoga studio. A deep dive into styled components.',
+    tags: ['React', 'CSS'],
+    demo: 'https://spectacular-pudding-f8c7fa.netlify.app/',
+    code: 'https://github.com/code-and-cats/project-design-handoff'
+  },
+  {
+    name: 'Happy thoughts',
+    img: '../img/happythoughts.png',
+    alt: 'the site shown on mobile',
+    description:
+      'My first fullstack project. A twitter-like site, but with happy thoughts only.',
+    tags: ['React', 'CSS', 'API', 'MongoDB', 'mongoose'],
+    demo: 'https://enchanting-halva-88064a.netlify.app/',
+    code: 'https://github.com/code-and-cats/project-happy-thoughts'
+  },
+  {
+    name: 'Pet shrimp order form',
+    img: '../img/shrimporder.png',
+    alt: 'the site shown on mobile',
+    description:
+      'A small order form to practice React states. Order your shrimp and get a confirmation!',
+    tags: ['React', 'CSS'],
+    demo: 'https://astounding-starship-c864d5.netlify.app/',
+    code: 'https://github.com/code-and-cats/project-survey'
+  },
+  {
+    name: 'Music releases',
+    img: '../img/musicreleases.png',
+    alt: 'the site shown on tablet',
+    description:
+      'Spotify clone fetching music releases from an open API. The 50 latest music releases... from then.',
+    tags: ['React', 'CSS', 'Pair-programming'],
+    demo: 'https://vio-and-fridas-music-release-project.netlify.app/',
+    code: 'https://github.com/Technigo/project-music-releases/pull/251'
+  },
+  {
+    name: 'Business site - Dynamic Duo',
+    img: '../img/dd.png',
+    alt: 'the site shown on desktop and mobile',
+    description:
+      'Content consulting and styling of a Wordpress site for an experienced event company in Sweden.',
+    tags: ['Wordpress'],
+    demo: 'http://dynamicduo.se/',
+    code: 'http://dynamicduo.se/'
+  },
+  {
+    name: 'Weather app',
+    img: '../img/weatherapp.png',
+    alt: 'hand holding a mobile phone with the app on it',
+    description:
+      'The weather app fetches weather data from whichever city the user chooses (but will always start in Reykjavik).',
+    tags: ['HTML', 'JavaScript', 'CSS', 'Pair-programming'],
+    demo: 'https://comfy-weatherapp-ea57d9.netlify.app/',
+    code: 'https://github.com/code-and-cats/project-weather-app'
+  },
+  {
+    name: 'Guess who - the game',
+    img: '../img/guesswoof.png',
+    alt: 'plant bot on a laptop with a leaf of a plant in a glass of water next to it',
+    description:
+      'A take on the board game "guess who" - but with dogs. Wearing hats.',
+    tags: ['HTML', 'JavaScript', 'CSS'],
+    demo: 'https://chipper-nasturtium-c0cb39.netlify.app/',
+    code: 'https://github.com/code-and-cats/project-guess-who'
+  },
+  {
+    name: 'Chatbot built in JavaScript',
+    img: '../img/plantbot.png',
+    alt: 'the game shown on desktop and mobile',
+    description:
+      'A conversational tool to help the user choose the perfect plant.',
+    tags: ['HTML', 'JavaScript', 'CSS'],
+    demo: 'https://vermillion-kashata-dd952d.netlify.app/',
+    code: 'https://github.com/code-and-cats/project-chatbot'
+  },
+  {
+    name: 'News site',
+    img: '../img/dailymews.png',
+    alt: 'the site shown on desktop, mobile and tablet',
+    description:
+      'My first project working with responsive cards, flexbox and grid.',
+    tags: ['HTML', 'CSS'],
+    demo: 'https://earnest-sorbet-0d6a88.netlify.app/',
+    code: 'https://github.com/code-and-cats/project-news-site'
+  }
 ]
 
 export default projectList
